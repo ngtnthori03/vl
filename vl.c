4a32a0f2314fecd480700c855e30df9cf6ac8c74
ngtnthori03 <ngtnthori03@gmail.com>
2024-04-11 17:45:46 +0700

    vlisp

---
+++ vl.c
@@ -0,0 +1,27 @@
+
+
+#include <stdio.h>
+
+struct vl {
+  voic* valuevls;
+  struct vl* vls;
+
+  enum vl_trait {
+    VL__META = 0,
+    VL__RAW = 1,
+    VL__INTEGER = (1 << 1),
+    
+
+    vl_NONE = -1
+  };
+
+  struct vl (*vl_eval)(struct vl*);
+  
+};
+
+int main(int argc. char const* args[]) {
+
+
+  
+  return 0;
+}
